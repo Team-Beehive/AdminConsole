0dd9e138831b7475ed61a8072dd65ff31ecd1028
TuckerN-Code <nultytucker@gmail.com>
2022-01-15 15:43:31 -0800

    Buildings Data Type created

---
+++ AdminConsole/AdminDatabaseFramework/BuildingData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminDatabaseFramework
+{
+    public class BuildingData
+    {
+        public string BuildingName { get; set; }
+        public List<string> BuildingMajors { get; set; }
+        public string BuildingName_Info { get; set; }
+        public List<string> BuildingRoom_Type { get; set; }
+        public string BuildingConstructionYear { get; set; }
+    }
+}
---
+++ AdminConsole/AdminDatabaseFramework/Buildings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Cloud.Firestore;
+
+namespace AdminDatabaseFramework
+{
+    public class Buildings
+    {
+        public LinkedList<BuildingData> BuildingList { get; set; }
+        private FirestoreDb firestoreDb { get; set; }
+        public string project = "oit-kiosk";
+        public Buildings()
+        {
+            firestoreDb = FirestoreDb.Create(project);
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "C:\\Users\\nulty\\Documents\\JrProject\\Database\\oit-kiosk-firebase-adminsdk-u24sq-8f7958c50f.json");
+        }
+    }
+}
