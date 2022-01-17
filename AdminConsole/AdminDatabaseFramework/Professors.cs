e1e92087cbbd9b9047b1da9c23fc752d28b41d62
TuckerN-Code <nultytucker@gmail.com>
2022-01-17 11:44:06 -0800

    Create, Delete, and Get functions implemented

--- AdminConsole/AdminDatabaseFramework/Professors.cs
+++ AdminConsole/AdminDatabaseFramework/Professors.cs
@@ -14,6 +14,8 @@ namespace AdminDatabaseFramework
         public string professorOffice { get; set; }
         public string professorEmail { get; set; }
         public string professorPhoneNumber { get; set; }
+        public DocumentReference DocumentReferenceSelf { get; set; }
+        public string oldTitle { get; set; }
 
         public Dictionary<string, object> ToDictionary()
         {
@@ -30,11 +32,78 @@ namespace AdminDatabaseFramework
     {
         private FirestoreDb db;
         public string project = "oit-kiosk";
+        public LinkedList<ProfessorData> LocalProfessors;
 
         public Professors()
         {
-            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "F:\\CSTCode\\JP\\Database\\oit-kiosk-firebase-adminsdk-u24sq-8f7958c50f.json")
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "F:\\CSTCode\\JP\\Database\\oit-kiosk-firebase-adminsdk-u24sq-8f7958c50f.json");
             db = FirestoreDb.Create(project);
+        }
+
+        public void UpdateLocal()
+        {
+            LocalProfessors = Task<LinkedList<ProfessorData>>.Run(() => db_GetProfessors()).Result;
+        }
+
+        public void CreateProfessor(ProfessorData professor)
+        {
+            Task.Run(() => db_CreateProfessor(professor)).Wait();
+        }
+
+        public void RemoveProfessor(ProfessorData professor)
+        {
+            Task.Run(() => db_RemoveProfessor(professor)).Wait();
+        }
+
+        public void UpdateProfessor(ProfessorData professor)
+        {
+            if(professor.professorName != professor.oldTitle)
+            {
+                ChangeProfessorName(professor);
+            }
+            else
+            {
+
+            }
+
+        }
+
+        private void ChangeProfessorName(ProfessorData professor)
+        {
+
+        }
+
+        private async Task<LinkedList<ProfessorData>> db_GetProfessors()
+        {
+            LinkedList<ProfessorData> professorDatas = new LinkedList<ProfessorData>();
+            CollectionReference reference = db.Collection("pages").Document("Professors").Collection("Professors");
+            QuerySnapshot documents = await reference.GetSnapshotAsync();
+            foreach(DocumentSnapshot document in documents)
+            {
+                Dictionary<string, object> data = document.ToDictionary();
+
+                ProfessorData temp = new ProfessorData();
+                temp.professorDepartment = data["department"].ToString();
+                temp.professorEmail = data["email"].ToString();
+                temp.professorOffice = data["office"].ToString();
+                temp.professorPhoneNumber = data["phone_number"].ToString();
+                temp.professorName = document.Id.ToString();
+                temp.oldTitle = temp.professorName;
+                temp.DocumentReferenceSelf = document.Reference;
+                professorDatas.AddLast(temp);
+            }
+            return professorDatas;
+        }
+
+        private async Task db_RemoveProfessor(ProfessorData professor)
+        {
+            await db.Collection("pages").Document("Professors").Collection("Professors").Document(professor.professorName).DeleteAsync();
+        }
+
+        private async Task db_CreateProfessor(ProfessorData professor)
+        {
+            DocumentReference document = db.Collection("pages").Document("Professors").Collection("Professors").Document(professor.professorName);
+            await document.SetAsync(professor.ToDictionary());
         }
     }
 }
