69f99b6ec464ec473e49532d1408311d7f89e704
DezzyTheDragon <destinygreenhill@gmail.com>
2022-03-30 12:38:20 -0700

    Added Category select user controll and put dropdown that is populated

---
+++ AdminConsole/AdminConsole/CategorySelect.xaml.cs
@@ -0,0 +1,42 @@
+﻿using AdminDatabaseFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdminConsole
+{
+    /// <summary>
+    /// Interaction logic for CategorySelect.xaml
+    /// </summary>
+    public partial class CategorySelect : UserControl
+    {
+        AppData m_data;
+        public CategorySelect(AppData data)
+        {
+            m_data = data;
+            InitializeComponent();
+            PopulateDropdown();
+        }
+
+        private void PopulateDropdown()
+        {
+            foreach (MajorCategories cat in m_data.s_catList)
+            {
+                ComboBoxItem catItem = new ComboBoxItem();
+                catItem.Content = cat.categoryTitle;
+                CategoryDropdown.Items.Add(catItem);
+            }
+        }
+    }
+}
